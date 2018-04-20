40fee9128f2af969dfb0817b6cce702b83a95991
Padma Channal <padmahchannal@gmail.com>
2018-04-20 12:51:35 -0700

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,313 @@
+// Code Source : Udemy
+///////////////////////////////////////
+// Lecture: variables
+/*
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: variables 2
+/*
+var name = 'John';
+var age = 26;
+
+//console.log(name + age);
+//console.log(age + age);
+
+var job, isMarried;
+
+//console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+
+//var lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: operators
+/*
+var now = 2016;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+//2016 - 52
+//1964
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+//ageJohn = ageMark = 26
+//ageJohn = 26
+
+ageJohn++;
+ageMark *= 2;
+ageMark = ageMark * 2;
+
+console.log(ageJohn);
+console.log(ageMark);
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: if/else statements
+/*
+var name = 'John';
+var age = 26;
+var isMarried = 'yes';
+
+if (isMarried === 'yes') {
+    console.log(name + ' is married!');
+} else {
+    console.log(name + ' will hopefully marry soon :)');
+}
+
+isMarried = true;
+
+if(isMarried) {
+    console.log('YES!');
+} else {
+    console.log('NO!');
+}
+
+if(isMarried) {
+    console.log('YES!');
+}
+
+if (23 === "23") {
+    console.log('Something to print...');
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: boolean logic and switch
+/*
+var age = 20;
+
+if (age < 20) {
+    console.log('John is a teenager');
+} else if (age >= 20 && age < 30) {
+    console.log('John is a young man.');
+} else {
+    console.log('John is a man.');
+}
+
+
+var job = 'teacher';
+
+job = prompt('What does john do?');
+
+switch (job) {
+    case 'teacher':
+        console.log('John teaches kids.');
+        break;
+    case 'driver':
+        console.log('John drives a cab in Lisbon.');
+        break;
+    case 'cop':
+        console.log('John helps fight crime.');
+        break;
+    default:
+        console.log('John does something else.');
+}
+*/
+
+
+
+///////////////////////////////////////
+// CODING CHALLENGE 1
+
+/*
+John and a friend invented a simple game where the player with the highest value of his height (in centimeters) plus five times his age wins (what a silly game :)
+
+1. Create variables for the heights and ages of two friends and assign them some values
+2. Calculate their scores
+3. Decide who wins and print the winner to the console. Include the score in the string that you output to the console. Don't forget that there can be a draw (both players with the same score).
+
+4. EXTRA: Add a third player and now decide who wins. Hint: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+
+
+// SOLUTION:
+var heightJohn = 170;
+var heightMike = 195;
+var ageJohn = 36;
+var ageMike = 29;
+
+var scoreJohn = heightJohn + 5 * ageJohn;
+var scoreMike = heightMike + 5 * ageMike;
+
+
+if (scoreJohn > scoreMike) {
+    console.log('John wins the game with ' + scoreJohn + ' points!');
+} else if (scoreMike > scoreJohn) {
+    console.log('Mike wins the game with ' + scoreMike + ' points!');
+} else if (scoreJohn === scoreMike) {
+    console.log('There is a draw.');
+}
+
+var heightMary = 158;
+var ageMary = 31;
+var scoreMary = heightMary + 5 * ageMary;
+
+
+if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+    console.log('John wins the game with ' + scoreJohn + ' points!');
+} else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
+    console.log('Mike wins the game with ' + scoreMike + ' points!');
+} else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
+    console.log('Mary wins the game with ' + scoreMary + ' points!');
+} else {
+    console.log('It\'s a draw.');
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: Functions
+
+/*
+function calculateAge(yearOfBirth) {
+    var age = 2016 - yearOfBirth;
+    return age;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1969);
+var ageMary = calculateAge(1948);
+
+
+function yearsUntilRetirement(name, year) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    
+    if (retirement >= 0) {
+        console.log(name + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(name + ' is already retired.');
+    }
+}
+
+
+yearsUntilRetirement('John', 1990);
+yearsUntilRetirement('Mike', 1969);
+yearsUntilRetirement('Mary', 1948);
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: Statements and expressions
+/*
+function someFun(par) {
+    //code
+}
+
+var someFun = function(par) {
+    //code
+}
+
+//Expressions
+3 + 4;
+var x = 3;
+
+//statements
+if (x === 5) {
+    //do soemthing
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: Arrays
+/*
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+names[1] = 'Ben';
+console.log(names);
+
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+console.log(john);
+
+if (john.indexOf('teacher') === -1) {
+    console.log('John is NOT a teacher.');
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: Objects
+/*
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false
+};
+
+console.log(john.lastName);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+
+john.lastName = 'Miller';
+john['job'] = 'programmer';
+
+console.log(john);
+
+
+
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1969;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
+*/
+
